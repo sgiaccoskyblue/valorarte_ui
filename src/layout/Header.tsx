7988107e8b69d1907f7b9cf9b64edb799f4d2c97
Santiago <sgiaccobasso@gmail.com>
2021-10-09 21:56:34 +0200

    add carousel, refactor header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,39 +3,164 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
-import Box from '@mui/material/Box';
+import { makeStyles } from '@mui/styles';
+import { useState, useEffect } from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import Menu from '@mui/icons-material/Menu';
 
-const Header = () => {
-  return (
-    <React.Fragment>
-      <Toolbar sx={{ bgcolor: 'black', borderBottom: 1 }}>
-        <Typography color='white'>VALORARTE</Typography>
-        <div style={{ flex: 1 }}></div>
-        <Box mx={2}>
-          <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
-            CASTING
-          </Link>
-        </Box>
-        <Box mx={2}>
-          <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
-            NOSOTROS
-          </Link>
-        </Box>
-        <Box mx={2}>
-          <Link to='/' style={{ textDecoration: 'none' }}>
-            <Button
-              size='small'
-              variant='contained'
-              disableElevation
-              sx={{ bgcolor: 'darkviolet', color: 'white', borderRadius: '30px', paddingX: '30px' }}
-            >
-              Ingresar
-            </Button>
-          </Link>
-        </Box>
+const headersData = [
+  {
+    label: 'casting',
+    href: '/casting',
+  },
+  {
+    label: 'nosotros',
+    href: '/nosotros',
+  },
+  {
+    label: 'Ingresar',
+    href: '/ingresar',
+  },
+];
+
+const useStyles = makeStyles(() => ({
+  header: {
+    backgroundColor: 'black',
+  },
+  logo: {
+    fontFamily: 'Work Sans, sans-serif',
+    fontWeight: 600,
+    color: '#FFFEFE',
+    textAlign: 'left',
+  },
+  menuButton: {
+    fontFamily: 'Open Sans, sans-serif',
+    fontWeight: 700,
+    size: '18px',
+    marginLeft: '38px',
+  },
+  toolbar: {
+    display: 'flex',
+    backgroundColor: 'black',
+    justifyContent: 'space-between',
+  },
+  drawerContainer: {
+    padding: '20px 30px',
+    backgroundColor: 'black',
+    height: '100%',
+  },
+}));
+
+export default function Header() {
+  const { header, logo, menuButton, toolbar, drawerContainer } = useStyles();
+
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false,
+  });
+
+  const { mobileView, drawerOpen } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState(prevState => ({ ...prevState, mobileView: true }))
+        : setState(prevState => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+
+    window.addEventListener('resize', () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener('resize', () => setResponsiveness());
+    };
+  }, []);
+
+  const displayDesktop = () => {
+    return (
+      <Toolbar className={toolbar}>
+        {valorarteLogo}
+        <div>{getMenuButtons()}</div>
       </Toolbar>
-    </React.Fragment>
-  );
-};
+    );
+  };
 
-export default Header;
+  const displayMobile = () => {
+    const handleDrawerOpen = () => setState(prevState => ({ ...prevState, drawerOpen: true }));
+    const handleDrawerClose = () => setState(prevState => ({ ...prevState, drawerOpen: false }));
+
+    return (
+      <Toolbar className={header}>
+        <IconButton
+          {...{
+            edge: 'start',
+            color: 'inherit',
+            'aria-label': 'menu',
+            'aria-haspopup': 'true',
+            onClick: handleDrawerOpen,
+          }}
+        >
+          <Menu />
+        </IconButton>
+
+        <Drawer
+          {...{
+            anchor: 'left',
+            open: drawerOpen,
+            onClose: handleDrawerClose,
+          }}
+        >
+          <div className={drawerContainer}>{getDrawerChoices()}</div>
+        </Drawer>
+
+        <div>{valorarteLogo}</div>
+      </Toolbar>
+    );
+  };
+
+  const getDrawerChoices = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Link
+          {...{
+            to: href,
+            color: 'inherit',
+            style: { textDecoration: 'none', color: 'white' },
+            key: label,
+          }}
+        >
+          <MenuItem>{label}</MenuItem>
+        </Link>
+      );
+    });
+  };
+
+  const valorarteLogo = (
+    <Typography variant='h6' component='h1' className={logo}>
+      Valorarte
+    </Typography>
+  );
+
+  const getMenuButtons = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Button
+          {...{
+            key: label,
+            color: 'inherit',
+            to: href,
+            className: menuButton,
+          }}
+        >
+          {label}
+        </Button>
+      );
+    });
+  };
+
+  return <AppBar>{mobileView ? displayMobile() : displayDesktop()}</AppBar>;
+}
